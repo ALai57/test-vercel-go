a1be0154b8306247bbf592e22f7640f94e669b97
Andrew Lai <andrew.s.lai5@gmail.com>
2024-07-19 15:25:43 -0500

    rename -> index.go

--- my-module/api/index.go
+++ my-module/api/index.go
@@ -8,3 +8,7 @@ import (
 func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "Hello, World!")
 }
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello from Go!</h1>")
+}
